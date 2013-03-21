408ca2ac459a33a8a70ac3d7b0637b503456263f
mose <mose@mose.com>
2013-03-21 21:57:42 +0800

    no need for routes here

--- app.js
+++ app.js
@@ -4,10 +4,9 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , passport = require('passport')
+  , flash = require('connect-flash')
   , config = require('./config.json')
   , stylus = require("stylus")
   , nib = require("nib")
@@ -27,10 +26,12 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.set('title', 'Pickuploot');
   app.use(express.favicon());
+  app.use(express.cookieParser('keyboard cat'));
+  app.use(express.session({ cookie: { maxAge: 60000 }}));
+  app.use(flash());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(stylus.middleware(
     { src: __dirname + '/public'
@@ -45,8 +46,31 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res){
+  res.render('index.jade', {
+    title: 'Home',
+    message: req.flash('info'),
+    error: req.flash('error')
+  });
+});
+
+app.post('/', function(req, res){
+  res.redirect('/');
+});
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+    failureFlash: 'Invalid username or password.',
+    successFlash: 'Welcome!'
+  })
+);
+
+app.get('/logout', function(req, res) {
+  delete req.session.username;
+  res.redirect('/');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
