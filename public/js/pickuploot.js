6a6c9d9c7e8f3e0d30991ab8b97952d8bffbc66b
mose <mose@mose.com>
2013-03-23 06:33:21 +0800

    made delete button

---
+++ public/js/pickuploot.js
@@ -0,0 +1,21 @@
+var getByClass = function(className, parent=document) {
+  parent || (parent = document);
+  var descendants = parent.getElementsByTagName("*"), i=-1, e, result=[];
+  while (e = descendants[++i]) {
+    ((' '+(e['class'] || e.className)+' ').indexOf(' '+className+' ') > -1) && result.push(e);
+  }
+  return result;
+}
+
+document.addEventListener('DOMContentLoaded',function() {
+  var imgs = getByClass("img");
+  for (i=0;i<imgs.length;i++) {
+    console.log(imgs[i]);
+    imgs[i].addEventListener("mouseover", function(e) {
+      getByClass("delete",this)[0].style.display = "block";
+    });
+    imgs[i].addEventListener("mouseout", function(e) {
+      getByClass("delete",this)[0].style.display = "none";
+    });
+  }
+});
