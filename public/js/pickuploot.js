83fc8aee42338d365ff8df34a17c1b3e1a4c5446
mose <mose@mose.com>
2013-04-24 00:18:02 +0800

    some more modularity

--- public/js/pickuploot.js
+++ public/js/pickuploot.js
@@ -9,13 +9,53 @@ var getByClass = function(className, parent) {
 
 document.addEventListener('DOMContentLoaded',function() {
   var imgs = getByClass("img");
+  var dropzone = document.getElementById("drop");
+  var xhr = new XMLHttpRequest();
+
   for (i=0;i<imgs.length;i++) {
-    console.log(imgs[i]);
     imgs[i].addEventListener("mouseover", function(e) {
       getByClass("delete",this)[0].style.display = "block";
     });
     imgs[i].addEventListener("mouseout", function(e) {
       getByClass("delete",this)[0].style.display = "none";
     });
+  }
+
+  function handler(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    dragover(e);
+    var files = e.dataTransfer.files;
+    console.log(e.dataTransfer);
+    for (var i = 0, f; f = files[i]; i++) {
+      console.log(f);
+
+      uploadfile(f);
+    }
+  }
+  function dragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    dropzone.className = (e.type == "dragover" ? "hover" : "");
+  }
+  function uploadfile(file) {
+    if (xhr.upload && file.type.indexOf("image/") != -1 && file.size <= 1000000) {
+      xhr.open("POST", "/multi", true);
+      xhr.setRequestHeader("X_FILENAME", file.name);
+      xhr.addEventListener("load", uploadComplete, false);
+      xhr.send(file);
+    }
+
+  }
+
+  // Drap and drop upload
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    if (xhr.upload) {
+      dropzone.addEventListener("dragover", dragover, false);
+      dropzone.addEventListener("dragleave", dragover, false);
+      dropzone.addEventListener("drop", handler, false);
+    }
+
+
   }
 });
